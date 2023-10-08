db590b7902ec49d7785fc024918edc9166a70e4c
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-10-08 18:57:49 +0200

    challenge and bonus SUCCESFUL COMPLETED

--- script.js
+++ script.js
@@ -70,15 +70,15 @@ const checkWinner = (avgDolphins, avgKoalas) => {
 checkWinner(scoreDolphins, scoreKoalas)
 
 
-const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 
-const bills = [125, 555, 44];
+// const bills = [125, 555, 44];
 
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-console.log(bills, tips, totals);
+// console.log(bills, tips, totals);
 
 
 const mark = {
@@ -192,4 +192,33 @@ while (dice !== 6) {
     console.log(`You rolled a ${dice}`);
     dice = Math.trunc(Math.random() * 6) + 1;
     if (dice === 6) console.log(`You rolled a ${dice} so the loop is about to end...`);
+};
+
+//Challenge #4
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 }
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
+}
+
+console.log(bills, tips, totals);
+
+//BONUS
+
+const calcAvarage = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum / arr.length;
+};
+
+console.log(calcAvarage(totals));
