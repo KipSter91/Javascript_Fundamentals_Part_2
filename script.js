d70ef8960b7f0798953395b882ac51ce81819969
Zsolt Marku <zsolt.marku910221@gmail.com>
2023-10-08 17:20:01 +0200

    while loop

--- script.js
+++ script.js
@@ -176,4 +176,20 @@ for (let excersize = 1; excersize < 5; excersize++) {
             console.log(`${rep} reputations.`);
         };
     };
-};
+}
+
+//while loop
+let rep = 1;
+while (rep <= 10) {
+    console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`You rolled a ${dice} so the loop is about to end...`);
+}
